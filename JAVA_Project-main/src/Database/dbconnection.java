4601e82bd4ea3ff46627ff600c16aeac4f2d7649
Prabashi2003 <nuwaniprabhashi2003@gmail.com>
2025-11-04 02:10:25 +0530

    Files

---
+++ JAVA_Project-main/src/Database/dbconnection.java
@@ -0,0 +1,20 @@
+package Database;
+
+import java.sql.*;
+
+
+public class dbconnection {
+    private static final String URL = "jdbc:mysql://localhost:3307/finalproject";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
